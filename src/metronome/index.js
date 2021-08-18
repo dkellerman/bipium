0b5fc62ce06ea356d81fea4b2e7be515ed2ee9bd
David Kellerman <dkellerman@spotify.com>
2021-08-17 22:38:18 -0400

    fix readme

--- src/metronome/index.js
+++ src/metronome/index.js
@@ -1,14 +1,18 @@
 import { Metronome } from './Metronome';
 import { Clicker } from './Clicker';
 
-export function createMetronome(mSettings = {}, clickerSettings = {}, audioContext = new AudioContext()) {
+export function createMetronome(
+  mSettings = {},
+  clickerSettings = {},
+  audioContext = new AudioContext(),
+) {
   const clicker = new Clicker({ audioContext, ...clickerSettings });
   return new Metronome({
     timerFn: () => audioContext.currentTime,
     clicker,
     ...mSettings,
   });
-};
+}
 
 export { Metronome } from './Metronome';
 export { Clicker, DEFAULT_SOUNDS } from './Clicker';
